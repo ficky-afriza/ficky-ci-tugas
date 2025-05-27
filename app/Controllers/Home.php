2026007cb7ae992c3a5b88392627f2126d29dbea
ficky-afriza <fickyafriza12@gmail.com>
2025-05-27 08:31:15 +0700

    first commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controllers;
+use App\Models\ProductModel; 
+
+class Home extends BaseController
+{
+
+        protected $product;
+
+        function __construct()
+        {
+            helper('number');
+            helper('form');
+            $this->product = new ProductModel();
+        }
+
+        public function index()
+        {
+            $product = $this->product->findAll();
+            $data['product'] = $product;
+            return view('v_home', $data);
+        }
+
+        
+    
+}
